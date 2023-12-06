5e0b3ae1537ff0e6b16d1c717b4bb2fe8a426c80
dbusnel <dbusnel@icloud.com>
2023-12-06 00:27:15 -0500

    added flask functionality for users

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -35,6 +35,7 @@ def create_app():
     # Import the various Beluprint Objects
     from src.customers.customers import customers
     from src.products.products  import products
+    from src.users.users        import users
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
@@ -5,8 +6,8 @@ from src import db
 users = Blueprint('users', __name__)
 
 # define routes :>P
-#get all users
-@products.route('/users', methods=['GET'])
+# get all users -- admin functionality, not needed for project
+@users.route('/users', methods=['GET'])
 def get_users():
     #obtain cursor
     cursor = db.get_db().cursor()
@@ -20,3 +21,120 @@ def get_users():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+
+
+# get user with id
+@users.route('/users/<id>', methods=['GET', 'POST'])
+def get_users(id):
+    if request.method == 'GET':
+        #obtain cursor
+        cursor = db.get_db().cursor()
+        cursor.execute('SELECT * FROM USERS WHERE id = ' + str(id))
+        column_headers = [x[0] for x in cursor.description]
+        json_data = []
+        theData = cursor.fetchall()
+
+        for row in theData:
+            json_data.append(dict(zip(column_headers, row)))
+
+        return jsonify(json_data)
+    else:
+        return 'CONDUCTOR WE HAVE A PROBLEM', 405
+    
+# report a user
+@users.route('/users/<userid>/warnings/', methods=['POST'])
+def issue_report(userid):
+    if request.method == 'POST':
+        data = request.json
+
+        flag_id = data['flag_id']
+        description = data['description']
+        user_id = userid
+
+        query = 'insert into flags (flag_id, description, user_id) values ('
+        query += flag_id + ', '
+        query += description + ', '
+        query += user_id + ')'
+
+        current_app.logger.info(query)
+
+
+        cursor = db.get_db().cursor()
+        cursor.execute(query)
+        db.get_db().commit()
+
+# get user likes
+@users.route('/users/<userid>/likes/', methods=['GET', 'POST', 'DELETE'])
+def getLikes(userid):
+    data = request.json
+    current_app.logger.info(data)
+    if request.method == 'GET':
+        cursor = db.get_db().cursor()
+        cursor.execute('SELECT * FROM user_likes WHERE id = ' + str(userid))
+        column_headers = [x[0] for x in cursor.description]
+        json_data = []
+        theData = cursor.fetchall()
+
+        for row in theData:
+            json_data.append(dict(zip(column_headers, row)))
+
+        return jsonify(json_data)
+    elif request.method == 'POST':
+        user_id = userid
+        liked_id = data['liked_id']
+
+        query = 'insert into user_likes (user_id, liked_id) values ('
+        query += user_id + ', '
+        query += liked_id + ')'
+
+        current_app.logger.info(query)
+
+        cursor = db.get_db().cursor()
+        cursor.execute(query)
+        db.get_db().commit()
+    elif request.method == 'DELETE':
+        liked_id = data['liked_id']
+
+        query = 'DELETE FROM User_Likes where liked_id = ' + liked_id
+        cursor.execute(query)
+        db.get_db().commit()
+
+
+# get users who like the given user
+@users.route('/users/<id>/liked-by', methods=['GET'])
+def get_users(id):
+    #obtain cursor
+    cursor = db.get_db().cursor()
+
+    if request.method == 'GET':
+        cursor.execute('SELECT * from ' +
+        'User JOIN Symphony.User_Likes ON User.user_id = User_Likes.user_id '
+        + 'WHERE liked_id = ;' + str(id))
+        column_headers = [x[0] for x in cursor.description]
+        json_data = []
+        theData = cursor.fetchall()
+
+        for row in theData:
+            json_data.append(dict(zip(column_headers, row)))
+
+        return jsonify(json_data)
+
+# get 'matches' -- users who liked each other. Return the other users
+# get users who like the given user
+@users.route('/users/<id>/matches', methods=['GET'])
+def get_users(id):
+    #obtain cursor
+    cursor = db.get_db().cursor()
+    if request.method == 'GET':
+        cursor.execute('SELECT u1.liked_id from User_Likes u1 JOIN User_Likes u2 '
+         + 'ON u1.liked_id = u2.user_id where u1.user_id = ' + str(id) + ' and u1.user_id = u2.liked_id '
+         'and u2.user_id = u1.liked_id;')
+        column_headers = [x[0] for x in cursor.description]
+        json_data = []
+        theData = cursor.fetchall()
+
+        for row in theData:
+            json_data.append(dict(zip(column_headers, row)))
+
+        return jsonify(json_data)
